c552283c46494f4e16854c25898323fb53e46c51
clopezl09 <clopezl@uniquindio.edu.co>
2026-02-08 20:48:47 -0500

    Add files via upload

---
+++ Datosdeusuario/src/main/java/org/cristhianlopez/Main.java
@@ -0,0 +1,39 @@
+package org.cristhianlopez;
+
+import java.util.Scanner; //ESTA CLASE ME AYUDA A LEER DATOS DESDE EL TECLADO
+import java.util.*; // PAQUETE PARA QUE ACEPTE . COMO DECIMAL
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US); // FORZAMOS EL PUNTO COMO DECIMAL
+
+        // Leer texto
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = entrada.nextLine();
+
+        // Leer números
+        System.out.print("Ingresa tu edad: ");
+        int edad = entrada.nextInt();
+
+
+        //LEER CIUDAD
+        entrada.nextLine(); // ESTO ME AYUDA A CONSUMIR EL SALTO DE LINEA
+        System.out.print("Ingresa tu ciudad de nacimiento: ");
+        String ciudad = entrada.nextLine();
+
+
+        System.out.print("Ingresa tu promedio CON PUNTO: ");
+        double promedio = entrada.nextDouble();
+
+        // Mostrar los datos leídos
+        System.out.println("\n###### INFORMACION PERSONAL ######"); // \n BAJAR UNA LIENA LA CONSOLA
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Ciudad de nacimiento : " + ciudad);
+        System.out.println("Promedio: " + promedio);
+
+        entrada.close(); // Cerrar el Scanner
+    }
+}
